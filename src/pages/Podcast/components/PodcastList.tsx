b5d52eb265bf374d7e6bee0b30a92bde3742ebad
Ankit kumar <ankit.kumar.info22@gmail.com>
2024-08-16 21:37:48 +0530

    fixed podcast sequence

--- src/pages/Podcast/components/PodcastList.tsx
+++ src/pages/Podcast/components/PodcastList.tsx
@@ -3,13 +3,24 @@ import PodcastDisplay from "./PodcastDisplay";
 
 import useGetPodcast from "@/Feature/podcast/useGetPodcast";
 import { Podcast } from "@/Interface/podcast";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function PodcastList() {
   const {getPodcast,data:podcastData}=useGetPodcast();
+  const [sortedPodcastData,setSortedPodcastData]=useState<Podcast[]|null>(null);
   useEffect(()=>{
     getPodcast();
+   
   },[])
+  useEffect(()=>{
+    //sort podcast data according to time 
+    const sortedPodcastData=podcastData?.podcasts?.sort((a:Podcast,b:Podcast)=>new Date(b.created_at).getTime()-new Date(a.created_at).getTime());
+    setSortedPodcastData(sortedPodcastData);
+  },[podcastData])
+
+
+
+
   return (
     <div className="w-full" >
       <section>
@@ -24,7 +35,7 @@ function PodcastList() {
 
         <section className="flex flex-col justify-center items-center min-w-full mt-[50px] ">
 {
-           podcastData&& podcastData?.podcasts?.map((podcast:Podcast)=>{
+           podcastData&& sortedPodcastData?.map((podcast:Podcast)=>{
               return <PodcastDisplay key={podcast.id} podcast={podcast}/>
             })  
 }
